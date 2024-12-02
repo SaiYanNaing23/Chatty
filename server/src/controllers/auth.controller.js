5ab22a638fc93050459771c8de59b41bd36032d9
Sai Yan Naing <pesilay2372002@gmail.com>
2024-12-02 10:50:14 +0630

    server_setup

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+
+export const signup = async(req,res) => {
+    console.log('signup')
+}
+
+export const login = async(req,res) => {
+    console.log('signup')
+}
+
+export const logout = async(req,res) => {
+    console.log('signup')
+}
